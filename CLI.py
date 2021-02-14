73caddf53eea9aaf442665e03fe8fbcaced6476b
Hossein Habibi <68292834+Hossein-Habibi-2004@users.noreply.github.com>
2021-02-14 17:34:16 +0330

    Add condition to convert pdf

--- CLI.py
+++ CLI.py
@@ -4,12 +4,18 @@ def main():
     main_url = input("Enter Sample Url: ")
     lesson_name = input("Enter Lesson Name: ")
     last_page = int(input("Enter Last Page Number: "))
+    pdf = input("Are You Want Convert To PDF? [Y/N]")
     
-    downloader = Skyroom_Downloader(main_url, lesson_name, last_page)
+    downloader = Skyroom_Downloader(main_url, lesson_name, last_page, PrintValue)
     downloader.download_images()
     print("Download Images: 100%")
-    downloader.convert_to_pdf()
-    print("Convert To Pdf: 100%")
+    if pdf.lower() == "y":
+        downloader.convert_to_pdf()
+        print("Convert To Pdf: 100%")
+    input("Please [Enter] To Exit :)")
+
+def PrintValue(v):
+    print(f"Page: {v}")
 
 if __name__ == '__main__':
     main()
