2b8aff8ebf81961fbcb94c2f0ecf015dc7562aa6
Hossein Habibi <68292834+Hossein-Habibi-2004@users.noreply.github.com>
2021-02-14 17:31:24 +0330

    Add `value_func`

--- downloader.py
+++ downloader.py
@@ -2,33 +2,29 @@ import os
 import img2pdf
 import requests
 from glob import glob
+from settings import join_path, FILES_DIR
 
-join_path = os.path.join
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-if os.path.exists(join_path(DATA_DIR, self.name)) == False:
-    os.mkdir(join_path(BASE_DIR, "Files"))
-DATA_DIR = join_path(BASE_DIR, "Files")
 
 class Skyroom_Downloader:
-    def __init__(self, url: str, name: str, last_page: int):
+    def __init__(self, url: str, name: str, last_page: int, value_func):
         self.url = url
         self.name = name
         self.last_page = last_page
+        self.value_func = value_func
 
-        if os.path.exists(join_path(DATA_DIR, self.name)) == False:
-            os.mkdir(join_path(DATA_DIR, self.name))
+        if os.path.exists(join_path(FILES_DIR, self.name)) == False:
+            os.mkdir(join_path(FILES_DIR, self.name))
     
     def download_images(self) -> bool:
         for page in range(1, self.last_page+1):
             page = str(page).zfill(len(str(self.last_page)))
             self.__download_img(page)
-            print(f'Page:{page}')
+            self.value_func(page)
         return True
     
     def convert_to_pdf(self):
-        folder_path = join_path(DATA_DIR, self.name)
-        pdf_path = join_path(DATA_DIR, self.name, self.name+'.pdf')
+        folder_path = join_path(FILES_DIR, self.name)
+        pdf_path = join_path(FILES_DIR, self.name, self.name+'.pdf')
         imagelist = self.__list_of_photos(folder_path)
         with open(pdf_path, 'wb') as f:
             f.write(img2pdf.convert(imagelist))
@@ -37,7 +33,7 @@ class Skyroom_Downloader:
     def __download_img(self, page: str) -> bool:
         url = self.url + f"/images/P-{page}.jpg"
         name = f"{page}.jpg"
-        with open(join_path(DATA_DIR, self.name, name), 'wb') as download_file:
+        with open(join_path(FILES_DIR, self.name, name), 'wb') as download_file:
             response = requests.get(url, stream=True)
             total_length = response.headers.get('content-length')
             if total_length is None:  # no content length header
